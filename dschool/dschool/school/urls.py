f66188cd457fba7fb711d1415f332012e1f9d821
tenz0wo <igor_rom7011@bk.ru>
2022-10-27 11:03:12 +0300

    first commit

--- dschool/dschool/school/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('charter', views.charter, name='charter'),
-    path('faq', views.faq, name='faq'),
-    path('contacts', views.contacts, name='contacts'),
-]
--- dschool/school/admin 2.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import FAQS, Cards, Extreme
-
-admin.site.register(FAQS)
-admin.site.register(Cards)
-admin.site.register(Extreme)
