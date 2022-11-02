c89a0f3ae52ca35784fb8f24cc3e06820c9886fb
tenz0wo <igor_rom7011@bk.ru>
2022-11-02 10:00:36 +0300

    commit

--- dschool/dschool/urls.py
+++ dschool/dschool/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('school.urls')),
-    path('info/', include('description.urls')),
+    path('categories/', include('description.urls')),
 
 ]
 
